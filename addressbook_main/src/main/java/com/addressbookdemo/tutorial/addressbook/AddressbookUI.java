9ce314bfc1b410f44ce787bda7d8fb4550d49cc2
puneetbhatia77 <puneetbhatia77@gmail.com>
2021-02-14 11:34:16 +0530

    Update AddressbookUI.java

--- addressbook_main/src/main/java/com/addressbookdemo/tutorial/addressbook/AddressbookUI.java
+++ addressbook_main/src/main/java/com/addressbookdemo/tutorial/addressbook/AddressbookUI.java
@@ -30,7 +30,7 @@ import com.vaadin.v7.ui.TextField;
 public class AddressbookUI extends UI {
 
     /*
-     * Hundreds of widgets. Vaadin's user interface components are just Java
+     * Hundreds of widgets.... Vaadin's user interface components are just Java
      * objects that encapsulate and handle cross-browser support and
      * client-server communication. The default Vaadin components are in the
      * com.vaadin.ui package and there are over 500 more in
